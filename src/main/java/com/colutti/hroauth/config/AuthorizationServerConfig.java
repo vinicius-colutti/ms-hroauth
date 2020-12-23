c7d120899f23e6fb53b8003219572d053ff78edc
Vinicius Colutti <viniciuscolutti@hotmail.com>
2020-12-22 21:13:18 -0300

    adicionando configuracao dinamica

--- src/main/java/com/colutti/hroauth/config/AuthorizationServerConfig.java
+++ src/main/java/com/colutti/hroauth/config/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.colutti.hroauth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,6 +16,12 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+	
+	@Value("${oauth.client.name}")
+	private String oauthClientName;
+	
+	@Value("${oauth.client.secret}")
+	private String oauthClientSecret;
 
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
@@ -35,7 +42,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient("myappname123").secret(passwordEncoder.encode("myappsecret123"))
+		clients.inMemory().withClient(oauthClientName).secret(passwordEncoder.encode(oauthClientSecret))
 				.scopes("read", "write").authorizedGrantTypes("password").accessTokenValiditySeconds(86400);
 
 	}
